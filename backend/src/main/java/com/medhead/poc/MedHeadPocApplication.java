e4aaed6e218aa411d43ff567746767d4de71ecc6
Eric <contact@eric-gigondan.fr>
2026-07-17 11:41:45 +0200

    chore: scaffold backend Maven/Spring Boot project

---
+++ backend/src/main/java/com/medhead/poc/MedHeadPocApplication.java
@@ -0,0 +1,12 @@
+package com.medhead.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedHeadPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedHeadPocApplication.class, args);
+    }
+}
